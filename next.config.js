2629a7dc4809c79990dbea70a9a1abded36c3e90
vasiljevsvladislavs-dot <vasiljevs.vladislavs@gmail.com>
2025-10-21 00:43:39 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone', // ← Важно! серверный билд
+  // серверный бандл, НЕ 'export'
+  output: 'standalone',
 };
-
 module.exports = nextConfig;
