2fe50c9d7ee041e63aef72106bb52de07df56143
vasiljevsvladislavs-dot <vasiljevs.vladislavs@gmail.com>
2025-10-21 08:37:09 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,8 @@
 import type { Config } from "tailwindcss";
-const config: Config = { content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"], theme: { extend: {} }, plugins: [] };
+
+const config: Config = {
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+};
 export default config;
